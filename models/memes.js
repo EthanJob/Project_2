eda04f162f1276c5882c5d4817bf70c8546f69ff
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-12-04 13:51:51 -0500

    date stamps made for new memes created

--- models/memes.js
+++ models/memes.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const memeSchema = mongoose.Schema({
   posted_by: { type: String, required: true },
-  url: { type: String, required: true }
+  url: { type: String, required: true },
+  date_posted: { type: String }
 });
 
 module.exports = mongoose.model('Meme', memeSchema);
